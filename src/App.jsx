fbdea83bee460ea776d6db49951b2355e0145cc6
Boluwatife Omoloye <omoloyeamoss65@gmail.com>
2025-09-29 15:59:02 +0100

    Add 404.html for SPA routing on GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { ProtectedRoute } from './Components/ProtectedRoute'
 import HomePage from './Pages/HomePage'
 import Navbar from './Components/Navbar'
